b7c36f3a7838b3c6f4687bc011eae503a27f7558
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-12-09 00:33:21 +0300

    update

--- src/models/products/productColor.models.ts
+++ src/models/products/productColor.models.ts
@@ -1,39 +1,47 @@
-import mongoose from "mongoose"
-import ProductSchema, { ProductType } from "./product.model"
+import mongoose from 'mongoose';
+import ProductSchema, { ProductType } from './product.model';
 
 type ProductColorType = {
-    rateBase: number
-    rateConnection: number
-    priceBase: number
-    sort: number
-} & ProductType
+  rateBase: number;
+  rateConnection: number;
+  priceBase: number;
+  priceSection: number;
+  sort: number;
+} & ProductType;
 
-const schema = new mongoose.Schema<ProductColorType>({
+const schema = new mongoose.Schema<ProductColorType>(
+  {
     ...ProductSchema.obj,
     rateBase: {
-        type: Number,
-        required: false,
+      type: Number,
+      required: false,
     },
     rateConnection: {
-        type: Number,
-        required: false,
+      type: Number,
+      required: false,
     },
     priceBase: {
-        type: Number,
-        required: false,
+      type: Number,
+      required: false,
+    },
+    priceSection: {
+      type: Number,
+      required: false,
     },
     sort: {
-        type: Number,
-        required: true
-    }
-}, {
+      type: Number,
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-    versionKey: false
-})
+    versionKey: false,
+  }
+);
 
-schema.index({ group: 1, code: 1 }, { unique: true })
+schema.index({ group: 1, code: 1 }, { unique: true });
 
-const ProductColor = mongoose.model('ProductColor', schema)
+const ProductColor = mongoose.model('ProductColor', schema);
 
-export default ProductColor
-export type { ProductColorType }
+export default ProductColor;
+export type { ProductColorType };
--- src/modules/moysklad/utils/getRoomItemPriceX100.ts
+++ src/modules/moysklad/utils/getRoomItemPriceX100.ts
@@ -1,57 +1,91 @@
-import { ProjectCurrencies } from "../types/currencies.type"
-import { RoomItem } from "../types/roomItem.type"
-import ProductAccessory from '../../../models/products/productAccessory.models'
-import ProductModel from '../../../models/products/productModel.models'
-import ProductConnection from '../../../models/products/productConnection.models'
-import ProductColor from '../../../models/products/productColor.models'
+import { ProjectCurrencies } from '../types/currencies.type';
+import { RoomItem } from '../types/roomItem.type';
+import ProductAccessory from '../../../models/products/productAccessory.models';
+import ProductModel from '../../../models/products/productModel.models';
+import ProductConnection from '../../../models/products/productConnection.models';
+import ProductColor from '../../../models/products/productColor.models';
 
 type Props = {
-    item: RoomItem
-    currencies: ProjectCurrencies
-}
-
+  item: RoomItem;
+  currencies: ProjectCurrencies;
+};
 
 const getRoomItemPriceX100 = async (props: Props): Promise<number> => {
+  const dbItem = props.item.accessory
+    ? await ProductAccessory.findOne({ _id: props.item.accessory })
+    : await ProductModel.findOne({ _id: props.item.model });
+  if (!dbItem) return 0;
 
-    const dbItem = props.item.accessory ?
-        await ProductAccessory.findOne({ _id: props.item.accessory }) :
-        await ProductModel.findOne({ _id: props.item.model })
-    if (!dbItem) return 0
+  const currencyRate =
+    dbItem.currency === 'rub'
+      ? 1
+      : dbItem.currency === 'eur'
+      ? props.currencies.eur
+      : dbItem.currency === 'varmann'
+      ? props.currencies.varmann
+      : 0;
 
-    const currencyRate = dbItem.currency === 'rub' ? 1 :
-        dbItem.currency === 'eur' ? props.currencies.eur :
-            dbItem.currency === 'varmann' ? props.currencies.varmann : 0
+  let price =
+    ((dbItem.priceBase || 0) +
+      //@ts-ignore
+      (dbItem.priceSection || 0) * +(props.item.sections || 0)) *
+    currencyRate;
 
+  let connectionPrice = 0;
+  if (props.item.connection) {
+    const dbConnection = await ProductConnection.findOne({
+      _id: props.item.connection,
+    });
+    if (!!dbConnection) {
+      const connCurrencyRate =
+        dbConnection.currency === 'rub'
+          ? 1
+          : dbConnection.currency === 'eur'
+          ? props.currencies.eur
+          : dbConnection.currency === 'varmann'
+          ? props.currencies.varmann
+          : 0;
+      connectionPrice = dbConnection.priceBase * connCurrencyRate;
+    }
+  }
 
-    let price = (
-        (dbItem.priceBase || 0) +
-        //@ts-ignore
-        (dbItem.priceSection || 0) * +(props.item.sections || 0)
-    ) * currencyRate
+  if (props.item.color) {
+    const dbColor = await ProductColor.findOne({ _id: props.item.color });
 
+    const colorCurrencyRate = dbColor
+      ? dbColor.currency === 'rub'
+        ? 1
+        : dbColor.currency === 'eur'
+        ? props.currencies.eur
+        : dbColor.currency === 'varmann'
+        ? props.currencies.varmann
+        : 0
+      : 0;
 
-    let connectionPrice = 0
-    if (props.item.connection) {
-        const dbConnection = await ProductConnection.findOne({ _id: props.item.connection })
-        if (!!dbConnection) {
-            const connCurrencyRate = dbConnection.currency === 'rub' ? 1 :
-                dbConnection.currency === 'eur' ? props.currencies.eur :
-                    dbConnection.currency === 'varmann' ? props.currencies.varmann : 0
-            connectionPrice = dbConnection.priceBase * connCurrencyRate
-        }
+    if (dbColor?.priceBase) {
+      price = price + dbColor.priceBase * colorCurrencyRate;
     }
 
-    if (props.item.color) {
-        const dbColor = await ProductColor.findOne({ _id: props.item.color })
-        if (dbColor?.rateBase) {
-            price = price * dbColor.rateBase
-        }
-        if (dbColor?.rateConnection) {
-            connectionPrice = connectionPrice * dbColor.rateConnection
-        }
+    if (dbColor?.priceSection) {
+      price =
+        price +
+        dbColor.priceSection * colorCurrencyRate * +(props.item.sections || 0);
     }
 
-    return Math.floor((price + connectionPrice) * (100 - (props.item.discount || 0)) / 100) * 100
-}
+    if (dbColor?.rateBase) {
+      price = price * dbColor.rateBase;
+    }
 
-export default getRoomItemPriceX100
+    if (dbColor?.rateConnection) {
+      connectionPrice = connectionPrice * dbColor.rateConnection;
+    }
+  }
+
+  return (
+    Math.floor(
+      ((price + connectionPrice) * (100 - (props.item.discount || 0))) / 100
+    ) * 100
+  );
+};
+
+export default getRoomItemPriceX100;
