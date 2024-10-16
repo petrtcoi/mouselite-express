aa12caaa0671089bfcd16e186be37a356064824f
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2024-10-16 13:21:19 +0300

    update changes

--- src/modules/kradiator/getSiteModel.ts
+++ src/modules/kradiator/getSiteModel.ts
@@ -1,11 +1,10 @@
-import { Request, Response } from "express";
-import KradSiteModel from "./models/kradSiteModel.model"
+import { Request, Response } from 'express'
+import KradSiteModel from './models/kradSiteModel.model'
 
-const getSiteModel = async (req:Request, res: Response) => {
-
-    const modelId = req.params.model
-    const data = await KradSiteModel.findOne({id: modelId}).populate('radiators').lean()
-    res.status(200).send(data)
+const getSiteModel = async (req: Request, res: Response) => {
+	const modelId = req.params.model
+	const data = await KradSiteModel.findOne({ name: modelId }).populate('radiators').lean()
+	res.status(200).send(data)
 }
 
 export default getSiteModel
